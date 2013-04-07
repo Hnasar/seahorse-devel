1a0ead71227990b75183b8f3e9074e8735685326
Hashem Nasarat <hnasarat@gmail.com>
2013-04-07 15:17:43 -0400

    Fix g_option_context_new program summary

--- libseahorse/seahorse-application.c
+++ libseahorse/seahorse-application.c
@@ -135,7 +135,7 @@ seahorse_application_local_command_line (GApplication *application,
 
 	argc = g_strv_length (*arguments);
 
-	context = g_option_context_new (N_("- System Settings"));
+	context = g_option_context_new (N_("- Password and key manager"));
 	g_option_context_set_ignore_unknown_options (context, TRUE);
 	g_option_context_add_main_entries (context, options, GETTEXT_PACKAGE);
 	g_option_context_set_translation_domain(context, GETTEXT_PACKAGE);
